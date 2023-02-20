659b0e3afd499bfc50733eeabbed4f396be69df7
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-02-20 16:05:39 +0700

    feat: Add read and write method

--- src/models/model.js
+++ src/models/model.js
@@ -8,28 +8,43 @@ class Model {
     };
   }
 
+  read() {
+    return this.items;
+  }
+
+  write(item) {
+    this.items = item;
+  }
+
   create(item) {
-    this.items.ids.push(item.id);
-    this.items.data[item.id] = item;
-    return this.items.data[item.id];
+    const items = this.read();
+
+    items.ids.push(item.id);
+    items.data[item.id] = item;
+
+    this.write(items);
+
+    return items.data[item.id];
   }
 
   findById(itemId) {
-    const item = this.items.data[itemId];
+    const items = this.read();
+    const item = items.data[itemId];
     if (!item) return null;
     return item;
   }
   // the object filter use and logic for all properties.
   // looking an item that match all filter properties
   find(filter) {
+    const items = this.read();
     if (!filter) {
-      return this.items.ids.map((id) => this.items.data[id]);
+      return items.ids.map((id) => items.data[id]);
     } else {
       const foundItems = [];
-      this.items.ids.forEach((itemId) => {
-        const item = this.items.data[itemId];
-        const isMatch = this.checkIsMatch(item, filter);
-        if (isMatch) foundItems.push(this.items.data[itemId]);
+      items.ids.forEach((itemId) => {
+        const item = items.data[itemId];
+        const isMatch = checkIsMatch(item, filter);
+        if (isMatch) foundItems.push(items.data[itemId]);
       });
       return foundItems;
     }
@@ -57,38 +72,43 @@ class Model {
   }
 
   update(itemId, itemData) {
-    const task = this.items.data[itemId];
+    const items = this.read();
+    const task = items.data[itemId];
     if (!task) return null;
     for (const key in itemData) {
       if (Object.hasOwnProperty.call(task, key)) {
-        this.items.data[itemId][key] = itemData[key];
+        items.data[itemId][key] = itemData[key];
       }
     }
-    this.items.data[itemId].updatedAt = new Date();
-    return this.items.data[itemId];
+    items.data[itemId].updatedAt = new Date();
+    this.write(items);
+    return items.data[itemId];
   }
 
   // example {id: 'id', title: 'title'} default to && logic. so true if an object match all filter properties
   removeMany(filter) {
+    const items = this.read();
     // remove the id, remove the objects
     const deletedItems = [];
     // looping ids
-    this.items.ids.forEach((itemId) => {
-      const item = this.items.data[itemId];
-      let isMatch = this.checkIsMatch(item, filter);
+    items.ids.forEach((itemId) => {
+      const item = items.data[itemId];
+      let isMatch = checkIsMatch(item, filter);
       // the operation
       if (isMatch) {
-        deletedItems.push(this.items.data[itemId]);
-        delete this.items.data[itemId];
-        this.items.ids = this.items.ids.filter((id) => id !== itemId);
+        deletedItems.push(items.data[itemId]);
+        delete items.data[itemId];
+        items.ids = items.ids.filter((id) => id !== itemId);
       }
     });
+    this.write(items);
     return deletedItems;
   }
 
   remove(id) {
     const deletedItems = this.removeMany({ id });
     if (!deletedItems.length) return null;
+
     return deletedItems[0];
   }
 }
