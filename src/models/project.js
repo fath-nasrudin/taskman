0ec2300445f508f64667d3c3a8e19f25bb98f299
fath-nasrudin <fath.nasrudin.i@gmail.com>
2023-02-20 18:26:15 +0700

    Add support for localStorage

--- src/models/project.js
+++ src/models/project.js
@@ -7,14 +7,23 @@ class Project extends Schema {
   }
 }
 
-const projectStorage = {
+const tempStorage = {
   ids: [],
   data: {},
 };
+const STORAGE_NAME = 'projects';
+if (typeof Storage !== 'undefined') {
+  if (!localStorage.getItem(STORAGE_NAME)) {
+    localStorage.setItem(STORAGE_NAME, JSON.stringify(tempStorage));
+  }
+} else {
+  console.log('No support storage');
+}
+
 class ProjectModel extends Model {
   constructor() {
     super();
-    this.items = projectStorage;
+    this.items = tempStorage;
   }
   create(taskData) {
     const items = this.read();
@@ -24,6 +33,21 @@ class ProjectModel extends Model {
     this.write(items);
     return items.data[task.id];
   }
+}
+
+if (typeof Storage !== 'undefined') {
+  ProjectModel.prototype.read = function () {
+    const data = localStorage.getItem(STORAGE_NAME);
+    let parsedData = JSON.parse(data);
+    parsedData.ids.map((id) => {
+      parsedData.data[id] = new Project(parsedData.data[id]);
+    });
+    return parsedData;
+  };
+
+  ProjectModel.prototype.write = function (data) {
+    localStorage.setItem(STORAGE_NAME, JSON.stringify(data));
+  };
 }
 
 module.exports = new ProjectModel();
